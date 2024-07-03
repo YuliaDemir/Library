49b90c6731f9833a55a75e95be104b507d2edf52
Yulia  Demir <Nightelfin91@gmail.com>
2024-07-03 11:14:02 +0300

    the toString methos was added to the Book class

--- Library/src/Book.java
+++ Library/src/Book.java
@@ -2,5 +2,12 @@ public record Book(String title, String author, int pages,
                    String isbn, String isbnAuthor, String isbnTitle)
 {
 
-
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", pages=" + pages +
+                '}';
+    }
 }
